f39c31903e40be90da54af4cfa61a97eb98ca3a1
ZHP <1183017059@qq.com>
2020-12-08 19:51:10 +0800

    make the data folder a package

---
+++ data/__init__.py
@@ -0,0 +1,9 @@
+'''
+Author       : ZHP
+Date         : 2020-12-08 19:38:55
+LastEditors  : ZHP
+LastEditTime : 2020-12-08 19:38:56
+FilePath     : /Earlier_Project/data/__init__.py
+Description  : 项目说明
+Copyright 2020 ZHP
+'''
